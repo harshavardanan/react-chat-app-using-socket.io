9794e830f721933a448a12fa1ffe5f9db1ad0e00
Harshavardanan <68936029+harshavardanan@users.noreply.github.com>
2021-03-14 18:12:51 +0530

    Add files via upload

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Input.css";
+
+export default function Input({setMessage, sendMessage, message}) {
+    return (
+        <form className="input-form">
+            <input className="text-input" value={message} 
+            placeholder="Type your message!!!"
+            onChange={event => setMessage(event.target.value)}
+            onkeyPress={event => event.key==='Enter'? sendMessage : 'null'}
+            />
+            <button className="send-button" onClick={sendMessage}>SEND</button>
+        </form>
+    )
+}
